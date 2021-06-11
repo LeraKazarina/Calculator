2fce5cfdf97ef43ea122dd73bce6545ae6ac52c8
Lera Kazarina <lera.kazarina21@gmail.com>
2021-06-11 08:47:20 +0700

    Changed files

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,13 +10,7 @@ public class Calculator {
     BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0;
 
     UnaryOperator<Integer> pow = x -> x * x;
-    UnaryOperator<Integer> abs = x -> {
-        if (x > 0) {
-            return x;
-        } else {
-            return x * -1;
-        }
-    };
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
     Predicate<Integer> isPositive = x -> x > 0;
 
